06b15992ec13d16341e492ead69ff6a6d0262b95
moath <moath.shraim@gmail.com>
2018-08-30 17:33:23 +0300

    fill missing lines

--- ERC721Token.sol
+++ ERC721Token.sol
@@ -1,50 +1,35 @@
 pragma solidity ^0.4.24;
-
 import "./ERC721.sol";
 import "./ERC721BasicToken.sol";
 import "./SupportsInterfaceWithLookup.sol";
-
-
 contract ERC721Token is SupportsInterfaceWithLookup, ERC721BasicToken, ERC721 {
-
   // Token name
   string internal name_;
-
   // Token symbol
   string internal symbol_;
-
   // Mapping from owner to list of owned token IDs
   mapping(address => uint256[]) internal ownedTokens;
-
   // Mapping from token ID to index of the owner tokens list
   mapping(uint256 => uint256) internal ownedTokensIndex;
-
   // Array with all token ids, used for enumeration
   uint256[] internal allTokens;
-
   // Mapping from token id to position in the allTokens array
   mapping(uint256 => uint256) internal allTokensIndex;
-
   // Optional mapping for token URIs
   mapping(uint256 => string) internal tokenURIs;
-
   /**
    * @dev Constructor function
    */
   constructor(string _name, string _symbol) public {
     name_ = _name;
     symbol_ = _symbol;
-
     // register the supported interfaces to conform to ERC721 via ERC165
     _registerInterface(InterfaceId_ERC721Enumerable);
     _registerInterface(InterfaceId_ERC721Metadata);
-
     for (uint256 i; i < 12; i++) {
       _mint(msg.sender, i);
     }
-
   }
-
   /**
    * @dev Gets the token name
    * @return string representing the token name
@@ -52,7 +37,6 @@ contract ERC721Token is SupportsInterfaceWithLookup, ERC721BasicToken, ERC721 {
   function name() external view returns (string) {
     return name_;
   }
-
   /**
    * @dev Gets the token symbol
    * @return string representing the token symbol
@@ -60,7 +44,6 @@ contract ERC721Token is SupportsInterfaceWithLookup, ERC721BasicToken, ERC721 {
   function symbol() external view returns (string) {
     return symbol_;
   }
-
   /**
    * @dev Returns an URI for a given token ID
    * Throws if the token ID does not exist. May return an empty string.
@@ -70,7 +53,6 @@ contract ERC721Token is SupportsInterfaceWithLookup, ERC721BasicToken, ERC721 {
     require(exists(_tokenId));
     return tokenURIs[_tokenId];
   }
-
   /**
    * @dev Gets the token ID at a given index of the tokens list of the requested owner
    * @param _owner address owning the tokens list to be accessed
@@ -88,7 +70,6 @@ contract ERC721Token is SupportsInterfaceWithLookup, ERC721BasicToken, ERC721 {
     require(_index < balanceOf(_owner));
     return ownedTokens[_owner][_index];
   }
-
   /**
    * @dev Gets the total amount of tokens stored by the contract
    * @return uint256 representing the total amount of tokens
@@ -96,7 +77,6 @@ contract ERC721Token is SupportsInterfaceWithLookup, ERC721BasicToken, ERC721 {
   function totalSupply() public view returns (uint256) {
     return allTokens.length;
   }
-
   /**
    * @dev Gets the token ID at a given index of all the tokens in this contract
    * Reverts if the index is greater or equal to the total number of tokens
@@ -107,7 +87,6 @@ contract ERC721Token is SupportsInterfaceWithLookup, ERC721BasicToken, ERC721 {
     require(_index < totalSupply());
     return allTokens[_index];
   }
-
   /**
    * @dev Internal function to set the token URI for a given token
    * Reverts if the token ID does not exist
@@ -118,7 +97,6 @@ contract ERC721Token is SupportsInterfaceWithLookup, ERC721BasicToken, ERC721 {
     require(exists(_tokenId));
     tokenURIs[_tokenId] = _uri;
   }
-
   /**
    * @dev Internal function to add a token ID to the list of a given address
    * @param _to address representing the new owner of the given token ID
@@ -126,10 +104,11 @@ contract ERC721Token is SupportsInterfaceWithLookup, ERC721BasicToken, ERC721 {
    */
    //number 15
   function addTokenTo(address _to, uint256 _tokenId) internal {
-    // YOUR CODE HERE
-
+    super.addTokenTo(_to,_tokenId);
+    uint256 length = ownedTokens[_to].length;
+    ownedTokens[_to].push(_tokenId);
+    ownedTokensIndex[_tokenId] = length;
   }
-
   /**
    * @dev Internal function to remove a token ID from the list of a given address
    * @param _from address representing the previous owner of the given token ID
@@ -137,25 +116,20 @@ contract ERC721Token is SupportsInterfaceWithLookup, ERC721BasicToken, ERC721 {
    */
   function removeTokenFrom(address _from, uint256 _tokenId) internal {
     super.removeTokenFrom(_from, _tokenId);
-
     // To prevent a gap in the array, we store the last token in the index of the token to delete, and
     // then delete the last slot.
     uint256 tokenIndex = ownedTokensIndex[_tokenId];
     uint256 lastTokenIndex = ownedTokens[_from].length.sub(1);
     uint256 lastToken = ownedTokens[_from][lastTokenIndex];
-
     ownedTokens[_from][tokenIndex] = lastToken;
     // This also deletes the contents at the last position of the array
     ownedTokens[_from].length--;
-
     // Note that this will handle single-element arrays. In that case, both tokenIndex and lastTokenIndex are going to
     // be zero. Then we can make sure that we will remove _tokenId from the ownedTokens list since we are first swapping
     // the lastToken to the first position, and then dropping the element placed in the last position of the list
-
     ownedTokensIndex[_tokenId] = 0;
     ownedTokensIndex[lastToken] = tokenIndex;
   }
-
   /**
    * @dev Internal function to mint a new token
    * Reverts if the given token ID already exists
@@ -164,10 +138,11 @@ contract ERC721Token is SupportsInterfaceWithLookup, ERC721BasicToken, ERC721 {
    */
    // number 16
   function _mint(address _to, uint256 _tokenId) internal {
-    // YOUR CODE HERE
-
+    super._mint(_to,_tokenId);
+    
+    allTokensIndex[_tokenId] = allTokens.length;
+    allTokens.push(_tokenId);
   }
-
   /**
    * @dev Internal function to burn a specific token
    * Reverts if the token does not exist
@@ -176,23 +151,18 @@ contract ERC721Token is SupportsInterfaceWithLookup, ERC721BasicToken, ERC721 {
    */
   function _burn(address _owner, uint256 _tokenId) internal {
     super._burn(_owner, _tokenId);
-
     // Clear metadata (if any)
     if (bytes(tokenURIs[_tokenId]).length != 0) {
       delete tokenURIs[_tokenId];
     }
-
     // Reorg all tokens array
     uint256 tokenIndex = allTokensIndex[_tokenId];
     uint256 lastTokenIndex = allTokens.length.sub(1);
     uint256 lastToken = allTokens[lastTokenIndex];
-
     allTokens[tokenIndex] = lastToken;
     allTokens[lastTokenIndex] = 0;
-
     allTokens.length--;
     allTokensIndex[_tokenId] = 0;
     allTokensIndex[lastToken] = tokenIndex;
   }
-
 }
